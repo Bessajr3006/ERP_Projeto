2782fc845973f288f4d5386d9f8b3910aab76442
Bessajr3006 <bessa3006@me.com>
2026-06-10 00:44:40 -0300

    Bump build version to bust cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference lib="webworker" />
 const sw = self;
-const CACHE_NAME = 'keystone-pwa-v20260609174042';
+const CACHE_NAME = 'keystone-pwa-v20260610004356';
 const ASSETS_TO_CACHE = [
     '/index.html',
     '/register.html',

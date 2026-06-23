fcc13e2927dda73ac2ae3530adcc5d2634b84e6d
Bessajr3006 <bessa3006@me.com>
2026-06-23 20:59:17 -0300

    fix(customers): make results footer functional using GridSummaryFooter schema

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference lib="webworker" />
 const sw = self;
-const CACHE_NAME = 'keystone-pwa-v20260623164000';
+const CACHE_NAME = 'keystone-pwa-v20260623205250';
 const ASSETS_TO_CACHE = [
     '/index.html',
     '/register.html',

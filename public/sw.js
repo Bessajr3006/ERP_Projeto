a1352a2523e19ea85c6765c19dffd4fa4884b058
Bessajr3006 <bessa3006@me.com>
2026-06-26 18:33:51 -0300

    style: remove icon and refine font color for clear filters button

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference lib="webworker" />
 const sw = self;
-const CACHE_NAME = 'keystone-pwa-v20260626150027';
+const CACHE_NAME = 'keystone-pwa-v20260626182940';
 const ASSETS_TO_CACHE = [
     '/index.html',
     '/register.html',

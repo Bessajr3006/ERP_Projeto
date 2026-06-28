10bd93f8b8cd02a203f8c9ee095d99b423512fa3
Bessajr3006 <bessa3006@me.com>
2026-06-28 13:44:27 -0300

    fix: rename menu item to Configurar Whatsapp and add CSS-driven hover rules

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 /// <reference lib="webworker" />
 const sw = self;
-const CACHE_NAME = 'keystone-pwa-v20260628024729';
+const CACHE_NAME = 'keystone-pwa-v20260628025141';
 const ASSETS_TO_CACHE = [
     '/index.html',
     '/register.html',
